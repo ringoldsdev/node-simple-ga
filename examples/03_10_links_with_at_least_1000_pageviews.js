f66743aabe6d41626cdc4d9fe343fcbcf0e101ed
Ringolds Lescinskis <ringolds.lescinskis@gmail.com>
2018-07-12 18:39:06 +0300

    Added example 3 - get first 10 links with at least 1000 pageviews

---
+++ examples/03_10_links_with_at_least_1000_pageviews.js
@@ -0,0 +1,33 @@
+require("dotenv").load();
+
+const path = require("path");
+const { GAOOP, RequestBuilder, MetricFilterBuilder } = require("../index.js");
+
+(async function() {
+	var GoogleAnalytics = new GAOOP({
+		keyFile: path.join(__dirname, "../key.json")
+	});
+
+	var request = new RequestBuilder();
+
+	request
+		.view(process.env.GA_VIEW_ID)
+		.pageSize(10)
+		.dimension("pagePath")
+		.metric("pageviews")
+		.orderAsc("pageviews");
+
+	var pageviewsFilter = new MetricFilterBuilder();
+	pageviewsFilter.metric("pageviews").lessThan(999).inverse();
+
+	request.metricFilter(pageviewsFilter);
+
+	try {
+		// Make the request and fetch data
+		var data = await GoogleAnalytics.run(request);
+		console.log(data);
+		// ToDo  Pagination
+	} catch (err) {
+		console.error(err);
+	}
+})();
