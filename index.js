5046389d92e813260eb0325c78e4cd5097289e14
Ringolds Lescinskis <ringolds.lescinskis@gmail.com>
2018-07-12 15:36:51 +0300

    Code cleanup - makeRequest function was moved outside of run function to make code more readable

--- index.js
+++ index.js
@@ -23,64 +23,71 @@ const GAOOP = function(params) {
 	this.analytics = google.analyticsreporting("v4");
 };
 
-GAOOP.prototype.run = function(request, params = {}) {
-	var that = this;
-	var currentPage = params.currentPage ? params.currentPage : 1;
+const makeRequest = function(client, analytics, request) {
 	return new Promise(function(resolve, reject) {
-		var results = [];
-
-		that.analytics.reports.batchGet(
+		analytics.reports.batchGet(
 			{
-				auth: that.client,
+				auth: client,
 				resource: {
 					reportRequests: [request.make()]
 				}
 			},
-			async function(err, response) {
+			function(err, response) {
 				if (err) {
 					return reject(err);
 				}
-
-				var report = response.data.reports[0];
-
-				report.data.rows.forEach(function(entry) {
-					results.push({
-						dimensions: Object.assign.apply(
-							{},
-							report.columnHeader.dimensions.map((v, i) => ({
-								[v.substring(3)]: entry.dimensions[i]
-							}))
-						),
-						metrics: Object.assign.apply(
-							{},
-							report.columnHeader.metricHeader.metricHeaderEntries.map((v, i) => ({
-								[v.name.substring(3)]: entry.metrics[0].values[i]
-							}))
-						)
-					});
-				});
-
-				if (!params.pages) {
-					return resolve(results);
-				}
-
-				if (currentPage === params.pages) {
-					return resolve(results);
-				}
-
-				if (report.nextPageToken) {
-					request.setPageToken(report.nextPageToken);
-					results = results.concat(
-						await that.run(request, {
-							pages: params.pages,
-							currentPage: currentPage + 1
-						})
-					);
-				}
-
-				resolve(results);
+				resolve(response);
 			}
 		);
+	});
+};
+
+GAOOP.prototype.run = function(request, params = {}) {
+	var that = this;
+	var currentPage = params.currentPage ? params.currentPage : 1;
+	return new Promise(async function(resolve, reject) {
+		var data = [];
+
+		var response = await makeRequest(that.client, that.analytics, request);
+
+		var report = response.data.reports[0];
+
+		report.data.rows.forEach(function(entry) {
+			data.push({
+				dimensions: Object.assign.apply(
+					{},
+					report.columnHeader.dimensions.map((v, i) => ({
+						[v.substring(3)]: entry.dimensions[i]
+					}))
+				),
+				metrics: Object.assign.apply(
+					{},
+					report.columnHeader.metricHeader.metricHeaderEntries.map((v, i) => ({
+						[v.name.substring(3)]: entry.metrics[0].values[i]
+					}))
+				)
+			});
+		});
+
+		if (!params.pages) {
+			return resolve(data);
+		}
+
+		if (currentPage === params.pages) {
+			return resolve(data);
+		}
+
+		if (report.nextPageToken) {
+			request.setPageToken(report.nextPageToken);
+			data = data.concat(
+				await that.run(request, {
+					pages: params.pages,
+					currentPage: currentPage + 1
+				})
+			);
+		}
+
+		resolve(data);
 	});
 };
 
