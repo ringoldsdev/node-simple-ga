3ee1da2f6ddf1e2bf7c7187f81d361f893ca1e48
Ringolds Lescinskis <ringolds.lescinskis@gmail.com>
2018-07-12 22:54:38 +0300

    Updated example 4

--- examples/04_top_links_not_containing.js
+++ examples/04_top_links_not_containing.js
@@ -1,30 +1,29 @@
 require("dotenv").load();
 
 const path = require("path");
-const { GAOOP, RequestBuilder, MetricFilterBuilder, DimensionFilterBuilder } = require("../index.js");
+const { SimpleGA, Request, DimensionFilter } = require("../index.js");
 
 (async function() {
-	var GoogleAnalytics = new GAOOP({
-		keyFile: path.join(__dirname, "../key.json")
-	});
+	var analytics = new SimpleGA(path.join(__dirname, "../key.json"));
 
-	var request = new RequestBuilder();
-
-	request
+	var request = (new Request())
 		.view(process.env.GA_VIEW_ID)
 		.pageSize(10)
 		.dimension("pagePath")
 		.metric("pageviews")
 		.orderDesc("pageviews");
+
 	
-	var pagePathFilter = new DimensionFilterBuilder();
-	pagePathFilter.dimension("pagePath").contains("/posts/").not();
+	var pagePathFilter = (new DimensionFilter())
+		.dimension("pagePath")
+		.contains("/posts/")
+		.not();
 
 	request.dimensionFilter(pagePathFilter);
 
 	try {
 		// Make the request and fetch data
-		var data = await GoogleAnalytics.run(request);
+		var data = await analytics.run(request);
 		console.log(data);
 		// ToDo  Pagination
 	} catch (err) {
