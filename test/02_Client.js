1edcb0246971f3bbaa4080e4c7280950ee37fe70
Ringolds Lescinskis <ringolds.lescinskis@gmail.com>
2018-07-14 14:26:03 +0300

    Adjusted Client tests

--- test/02_Client.js
+++ test/02_Client.js
@@ -9,17 +9,17 @@ describe("Client", function(){
 		cl.constructor.name.should.equal("JWT");
 		done();
 	});
+	var key = JSON.parse(fs.readFileSync("./key.json", "utf8"));
 	it("should create a client from key", function(done){
-		var key = JSON.parse(fs.readFileSync("./key.json", "utf8"))
 		var cl = Client.createFromKey(key);
 		cl.constructor.name.should.equal("JWT");
 		done();
 	});
 	it("should create a client from params", function(done){
-		var key = JSON.parse(fs.readFileSync("./key.json", "utf8"))
 		var cl = Client.createFromParams({
 			email: key.client_email,
-			privateKey: key.private_key
+			privateKey: key.private_key,
+			permissions: ["https://www.googleapis.com/auth/analytics.readonly"]
 		});
 		cl.constructor.name.should.equal("JWT");
 		done();
