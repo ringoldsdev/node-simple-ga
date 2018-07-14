cb0fd58c2263361589d5ddff47e1fc9771e28b69
Ringolds Lescinskis <ringolds.lescinskis@gmail.com>
2018-07-14 13:36:41 +0300

    ApiHelper test - Random string generation

--- test/01_ApiHelper.js
+++ test/01_ApiHelper.js
@@ -2,9 +2,14 @@ const should = require('chai').should();
 
 const ApiHelper = require("../src/ApiHelper");
 
+function randomString() {
+	return Math.random().toString(36).substr(2, 5);
+}
+
 describe("ApiHelper", function(){
-	it("should prepend passed value with ga:", function(done){
-		ApiHelper.generateApiName("test").should.equal("ga:test");
+	var str = randomString();
+	it("should prepend '"+str+"' with ga:", function(done){
+		ApiHelper.generateApiName(str).should.equal("ga:"+str);
 		done();
 	});
 });
