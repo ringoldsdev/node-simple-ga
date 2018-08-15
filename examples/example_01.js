34556a9bc65e9a428f3be8e6e826201f7720916e
Ringolds Lescinskis <ringolds.lescinskis@gmail.com>
2018-08-15 12:32:21 +0300

    Example update

--- examples/example_01.js
+++ examples/example_01.js
@@ -1,25 +1,16 @@
 require("dotenv").load();
 
-var moment = require('moment');
-
 const path = require("path");
 const { SimpleGA, Request } = require("../index.js");
 
 (async function() {
 	var analytics = new SimpleGA(path.join(__dirname, "../key.json"));
 
-	var startOf = moment().startOf('day').unix();
-
-	var request = new Request()
-		.select("pagepath","pagetitle","pageviews","sessions","users")
-		.dimension("dimension17")
+	var request = Request()
+		.select("pageviews","sessions","users")
 		.from(process.env.GA_VIEW_ID)
-		.where("pagepath").not().equals("/")
-		.whereDimension("dimension17").greaterThan(startOf)
-		.during("2018-07-23","2018-07-25")
-		.orderDesc("pageviews")
-		.unselect("pagepath","sessions","users")
-		.results(20)
+		.during("2018-07-20","2018-07-25")
+		.orderDesc("pageviews");
 
 	try {
 		var data = await analytics.run(request);
