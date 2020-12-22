88b70ba426cf9f36b9d1b5b9289879c220a01125
Ringolds Lescinskis <ringolds.lescinskis@gmail.com>
2020-12-22 23:22:49 +0200

    Using object-path-immutable instead of object-path

--- src/ObjectBuilder.js
+++ src/ObjectBuilder.js
@@ -1,9 +1,8 @@
-const objectPath = require("object-path");
+const objectPath = require("object-path-immutable");
 
 // Define all actions that can be called
 const ACTIONS_SET = "set";
 const ACTIONS_APPEND = "append";
-const ACTIONS_CONCAT = "concat";
 const ACTIONS_DELETE = "delete";
 const ACTIONS_REMOVE = "remove";
 
@@ -11,12 +10,10 @@ const ACTIONS_REMOVE = "remove";
 // You can specify either a function or string to have an alias
 const ACTION_HANDLERS = {
 	[ACTIONS_SET]: function(projection, data) {
-		objectPath.set(projection, data.key, data.value);
-		return projection;
+		return objectPath.set(projection, data.key, data.value);
 	},
 	[ACTIONS_DELETE]: function(projection, data) {
-		objectPath.del(projection, data.key);
-		return projection;
+		return objectPath.del(projection, data.key);
 	},
 	[ACTIONS_APPEND]: function(projection, data) {
 		if (!(data.key in projection)) {
@@ -25,10 +22,15 @@ const ACTION_HANDLERS = {
 		if (!Array.isArray(projection[data.key])) {
 			throw new Error(`Can't append to ${data.key}! It's not an array`);
 		}
-		let values = !Array.isArray(data.value) ? [data.value] : data.value;
-		values.forEach(function(value) {
-			objectPath.push(projection, data.key, value);
+
+		if(!Array.isArray(data.value)) {
+			return objectPath.push(projection, data.key, data.value);
+		}
+
+		data.value.forEach(function(value) {
+			projection = objectPath.push(projection, data.key, value);
 		});
+
 		return projection;
 	},
 	[ACTIONS_REMOVE]: ACTIONS_DELETE
